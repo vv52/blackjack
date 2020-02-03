a4d4d08c50887dd733e233f48d8d6b43c27b214c
Vanja <vv52@humboldt.edu>
2020-02-02 19:58:18 -0800

    added green background sprite and added button to deck code to allow drawing cards as a test. upon button press, the deck spawns a random int which is used to select a random card. before the int is used to select the card however, it is checked against a kept list of all cards that have been played, if it is found in the list, a new random number is generated. when the list becomes "full" (in this case, 52 card items), the deck sprite is turned off and no more cards can be drawn.

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public GameObject[] cards;
+    public GameObject deck;
+    private List<int> playedCards;
+    
+    private SpriteRenderer spriteRenderer;
+
+    bool deckEnd = false; 
+
+    void DrawCard()
+    {
+        int temp = Random.Range(0, 51);
+        
+        while (playedCards.IndexOf(temp) != -1 || playedCards.Count == 51)
+        {
+            if (playedCards.Count == 51)
+            {
+                deckEnd = true;
+                this.spriteRenderer = GetComponent<SpriteRenderer>();
+                this.spriteRenderer.enabled = false;
+                break;
+            }
+            temp = Random.Range(0, 51);
+        }
+        if (deckEnd == false)
+        {
+            GameObject playerCard = (GameObject)Instantiate(cards[temp]);
+            playerCard.SetActive(true);
+
+            playedCards.Add(temp);
+        }
+        else
+        {
+            //TODO: DeckReshuffle()
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playedCards = new List<int>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 28), "Draw"))
+        {
+            DrawCard();
+        }
+    }
+}
