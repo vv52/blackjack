e76b41ebb4849184dbea5b2af9f7547b62958caf
Vanja <vv52@humboldt.edu>
2020-02-02 22:20:50 -0800

    "Deal Cards" button now deals two cards to bottom of screen (for player) and two to top (for dealer). Can't seem to get one dealer card to deal face down yet.

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -7,12 +7,16 @@ public class Deck : MonoBehaviour
     public GameObject[] cards;
     public GameObject deck;
     private List<int> playedCards;
+    private List<int> playerCards;
+    private List<int> dealerCards;
+    public GameObject gameBoard;
     
     private SpriteRenderer spriteRenderer;
+    private CardModel cardModel;
 
     bool deckEnd = false; 
 
-    void DrawCard()
+    void PlayerDrawCard(int xPos, int yPos)
     {
         int temp = Random.Range(0, 51);
         
@@ -29,22 +33,58 @@ public class Deck : MonoBehaviour
         }
         if (deckEnd == false)
         {
-            GameObject playerCard = (GameObject)Instantiate(cards[temp]);
-            playerCard.SetActive(true);
-
+            GameObject playerCard = (GameObject)Instantiate(cards[temp], new Vector3(xPos, yPos, 0), Quaternion.identity);
+            playerCard.transform.SetParent(gameBoard.transform, false);
+            
             playedCards.Add(temp);
+            playerCards.Add(temp);
         }
         else
         {
             //TODO: DeckReshuffle()
         }
+    }
+
+    void DealerDrawCard(int xPos, int yPos, bool showFace)
+    {
+        int temp = Random.Range(0, 51);
         
+        while (playedCards.IndexOf(temp) != -1 || playedCards.Count == 51)
+        {
+            if (playedCards.Count == 51)
+            {
+                deckEnd = true;
+                this.spriteRenderer = GetComponent<SpriteRenderer>();
+                this.spriteRenderer.enabled = false;
+                break;
+            }
+            temp = Random.Range(0, 51);
+        }
+        if (deckEnd == false)
+        {
+            GameObject dealerCard = (GameObject)Instantiate(cards[temp], new Vector3(xPos, yPos, 0), Quaternion.identity);
+            dealerCard.transform.SetParent(gameBoard.transform, false);
+            if (showFace == false)
+            {
+                //cardModel = GetComponent<CardModel>();
+                //cardModel.ToggleFace(false);
+            }
+
+            playedCards.Add(temp);
+            dealerCards.Add(temp);
+        }
+        else
+        {
+            //TODO: DeckReshuffle()
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
         playedCards = new List<int>();
+        playerCards = new List<int>();
+        dealerCards = new List<int>();
     }
 
     // Update is called once per frame
@@ -57,7 +97,10 @@ public class Deck : MonoBehaviour
     {
         if (GUI.Button(new Rect(10, 10, 100, 28), "Deal Cards"))
         {
-            DrawCard();
+            PlayerDrawCard(-2, -1);
+            PlayerDrawCard(-1, -1);
+            DealerDrawCard(0, 1, true);
+            DealerDrawCard(1, 1, false);
         }
 
         //TODO: add more GUI elements for "Hit", "Stay", etc.
